b3f5960c3fa6a0233418abf545121c0ab2aae587
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-16 19:01:06 +0530

    refactor: remove Mainscreen and clean up index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-import Mainscreen from './screens/mainscreen'
 
 export default function index() {
   return (
-    <View style={styles.container}>
-     <Mainscreen/>
+    <View>
+      <Text>index</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
