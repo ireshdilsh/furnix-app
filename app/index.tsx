b18b7579deed300cc6db6e07aace5204feba4a2b
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-16 19:07:12 +0530

    Refactor code structure for improved readability and maintainability

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View>
+
+            </View>
+            <Image style={styles.image} source={require('../assets/images/landing-img.png')} />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    image: {
+        height: '100%',
+        width: '100%',
+        backgroundSize: 'cover'
+    }
+})
