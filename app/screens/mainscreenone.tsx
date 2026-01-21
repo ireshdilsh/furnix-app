31bdd31feec4a946101e446dcb664b00b3471926
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-21 09:33:38 +0530

    feat: remove unused import of useRoute in Mainscreenone component

--- app/screens/mainscreenone.tsx
+++ app/screens/mainscreenone.tsx
@@ -1,7 +1,6 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router';
-// import { useRoute } from '@react-navigation/native';
 
 export default function Mainscreenone() {
 
