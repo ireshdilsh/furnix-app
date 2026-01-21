f0b9f738fc34f8fd9ce209f0ddac29c1d8061dff
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-21 09:09:55 +0530

    feat: remove unused ActivityIndicator import in index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function index() {
 
