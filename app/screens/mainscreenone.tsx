cdd2a61e326d2e596beadc470824b894f7a20218
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-22 14:46:54 +0530

    feat: update Mainscreenone layout, replace images, and remove unused assets

--- app/screens/mainscreenone.tsx
+++ app/screens/mainscreenone.tsx
@@ -1,31 +1,44 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React, { useEffect } from 'react'
-import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react'
 
 export default function Mainscreenone() {
-
-    const router = useRouter()
-
   return (
-    <View style={styles.container}>
-      <Image style={styles.mainImage} source={require('../../assets/images/Furnix-removebg-preview.png')} />
-      
+    <View>
+      <View style={styles.circle_1}/>
+      <Text style={styles.title}>Find Your {'\n'}Perfect Furniture</Text>
+      <Image style={styles.img} source={require('../../assets/images/istockphoto-505973586-612x612.jpg')}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
 
-  container:{
-    flex: 1,
-    justifyContent:'center',
-    alignItems:'center'
+  circle_1:{
+    borderRadius:250,
+    height:150,
+    width:150,
+    borderColor:'#E5E7EB',
+    borderWidth:1.2,
+    position:'absolute',
+    left:-50,
+    top:50
   },
-    mainImage:{
-        width: 150,
-        height: 50,
-        marginTop:450
-    },
 
-   
+  img:{
+    marginTop:150,
+    height:'75%',
+    width:330,
+    marginLeft:80
+  },
+
+  title:{
+    position:'absolute',
+    left:30,
+    top:100,
+    fontFamily:'Roboto_500Medium',
+    fontSize:30,
+    zIndex:1
+  }
+
+
 })
