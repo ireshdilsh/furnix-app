1030ee5ee3ec49f9c6cdb97529ffebde92213f54
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-22 22:53:13 +0530

    feat: add network status display to Header component and update dependencies

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,38 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { useBatteryLevel } from 'expo-battery';
+import { useNetworkState } from 'expo-network';
 
 
 export default function Header() {
 
     const batteryLevel = useBatteryLevel();
+    const networkState = useNetworkState();
+
+    let netWorkIcon = ''
+
+    if (networkState?.type === 'WIFI') {
+        netWorkIcon = 'https://img.icons8.com/?size=100&id=60027&format=png&color=000000'
+    }
+
+    if (networkState?.type === 'CELLULAR') {
+        netWorkIcon = 'https://img.icons8.com/?size=100&id=83ir2E7qJHoB&format=png&color=000000'
+    }
+
+    if (networkState?.type === 'ETHERNET') {
+        netWorkIcon = 'https://img.icons8.com/?size=100&id=P3_jnk6ErQF5&format=png&color=000000'
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.dateTime}>
                 <Text style={styles.dateText}>{new Date().toString().substring(0, 10)}</Text>
-                <Text style={styles.timeText}>{new Date().toLocaleTimeString().substring(0, 4)}</Text>
+                <Text style={styles.timeText}>{new Date().toLocaleTimeString().substring(0, 5)}</Text>
             </View>
             <View style={styles.battery}>
+                <View style={styles.network}>
+                    <Image style={styles.networkIcon} source={{ uri: netWorkIcon }} />
+                </View>
                 <Image style={styles.batteryIcon} source={{ uri: 'https://img.icons8.com/?size=100&id=cI1MyTQfFWiL&format=png&color=#4a5565' }} />
                 <Text style={styles.batteryText}>{batteryLevel !== null ? `${Math.round(batteryLevel * 100)}%` : 'Unknown'}</Text>
             </View>
@@ -30,6 +49,16 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         gap: 4,
         width: '88%',
+    },
+
+    network: {
+        marginRight:3
+    },
+
+    networkIcon: {
+        width: 16,
+        height: 14,
+        tintColor: '#4a5565'
     },
 
     battery: {
@@ -64,7 +93,6 @@ const styles = StyleSheet.create({
 
     dateTime: {
         flexDirection: 'row',
-        alignItems: 'center',
         gap: 10
     }
 })
