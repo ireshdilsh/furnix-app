76e05b0de2664e068ef6afa80819c8712f360997
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-24 17:55:26 +0530

    feat: update Userdashboard layout with Header component and styling

--- app/screens/userdashboard.tsx
+++ app/screens/userdashboard.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Header from '@/components/header'
 
 export default function Userdashboard() {
   return (
-    <View>
-      <Text>userdashboard</Text>
+    <View style={styles.container}>
+        <Header/>
+        
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        
+    }
+})
