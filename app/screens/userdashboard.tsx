a73fad7b2ccae7057d659b65f950b7c282fc2cd0
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-24 17:52:03 +0530

    fix: correct component name casing for Userdashboard

--- app/screens/userdashboard.tsx
+++ app/screens/userdashboard.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function userdashboard() {
+export default function Userdashboard() {
   return (
     <View>
       <Text>userdashboard</Text>
