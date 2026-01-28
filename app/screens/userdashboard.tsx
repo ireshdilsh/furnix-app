10b3e7629bd55ba704b3480ccc6a458345292013
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-28 11:25:47 +0530

    fix: restructure Userdashboard layout and add search input

--- app/screens/userdashboard.tsx
+++ app/screens/userdashboard.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native'
 import React from 'react'
 import Header from '@/components/header'
 
@@ -7,7 +7,11 @@ export default function Userdashboard() {
         <View style={styles.maincontainer}>
             <Header />
             <View style={styles.container}>
-                <Text style={styles.title}>Your Personalized Furix{'\n'}Home Experience</Text>
+                <View style={styles.inputAndProfile}>
+                    <Text style={styles.title}>Your Personalized Furix{'\n'}Home Experience</Text>
+                    <Image source={{uri : ''}}/>
+                </View>
+                <TextInput style={styles.search} placeholder='Search'/>
             </View>
         </View>
     )
@@ -21,12 +25,30 @@ const styles = StyleSheet.create({
         width: '100%',
     },
 
+    search: {
+        height: 48,
+        width: '100%',
+        backgroundColor: '#F9FAFB',
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        marginTop:20,
+        borderColor: '#E5E7EB',
+        borderWidth: 0.5,
+    },
+
     title: {
-        fontSize: 25,
+        fontSize: 24,
         fontWeight: '700',
         color: '#4a5565',
         fontFamily: 'Roboto_700Bold',
-        marginBottom: 20,
+    },
+
+    inputAndProfile: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 20
     },
 
     container: {
