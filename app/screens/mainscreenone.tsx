0eb76336ce95be2ff8e32c78175ca0c3d80f790f
ireshdilsh <ireshdilshan46@gmail.com>
2026-01-30 00:10:56 +0530

    feat: add Google Sign-In integration and update dependencies

--- app/screens/mainscreenone.tsx
+++ app/screens/mainscreenone.tsx
@@ -1,30 +1,43 @@
 import Header from '@/components/header'
-import { auth } from '@/config/config'
+import { auth } from '../../config/config'
 import { useRouter } from 'expo-router'
-import { GoogleAuthProvider, signInWithCredential, signInWithPopup } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth'
+import React, { useState } from 'react'
 import { Image, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-// @ts-ignore
-import { WebBrowser } from 'expo-web-browser';
+import { getAuth, signInWithPopup } from "firebase/auth";
 
 export default function Mainscreenone() {
   const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false)
 
   const router = useRouter();
-
-  const gotoUserdashboard = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        console.log('User Info:', user);
-        router.push('/screens/userdashboard')
-      })
-      .catch((error) => {
-        console.error('Error during sign-in:', error);
-      });
-  };
-
+  const gotoUserdashboard = async () => {
+    try {
+      // Get the users ID token
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential?.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        });
+    } catch (error) {
+      console.log("Error during Google Sign-In:", error);
+    }
+  }
 
 
   return (
