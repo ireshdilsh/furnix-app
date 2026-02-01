70a331163a6f9f84eddd7c96b1cf8dc16150c9e5
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-01 20:33:05 +0530

    feat: update index component layout and add Login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { View, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import { useRouter } from 'expo-router';
 
 export default function index() {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.replace('/'); 
+        }, 5000);
+    });
+
     return (
-        <View>
-            <Text>index</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+            <Image source={require('../assets/images/FURNIX-removebg-preview.png')} style={{ width: 200, height: 200 }} />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-
-})
