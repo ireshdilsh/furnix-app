b9396ed9eb5d0b58bdfe1f2eef31973228f18699
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-01 20:23:54 +0530

    feat: format index component and manage image assets

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,13 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
