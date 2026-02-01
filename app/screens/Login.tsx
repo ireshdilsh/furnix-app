e43f384d329a0fea6c319bd5187b5775047f115d
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-01 21:07:01 +0530

    feat: update Login component layout and styling

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{
+      paddingHorizontal: 30,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+     <View style={{width: '100%',backgroundColor:'red', padding: 20, borderRadius: 10}}>
+      
+     </View>
     </View>
   )
 }
