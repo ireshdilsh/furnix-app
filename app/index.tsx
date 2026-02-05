3956fd938360af1b0eb6143cc7523882722db05c
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-05 23:52:20 +0530

    feat: update index component to use Josefin Sans and Pacifico fonts with improved layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useEffect } from 'react';
-import { View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 export default function index() {
 
     const [loaded, error] = useFonts({
-        'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+        'Josefin': require('../fonts/josefin/static/JosefinSans-Medium.ttf'),
+        'Josefin-Bold': require('../fonts/josefin/static/JosefinSans-Bold.ttf'),
+        'Pacifico': require('../fonts/pacifico/Pacifico-Regular.ttf'),
     });
 
     useEffect(() => {
@@ -21,8 +23,22 @@ export default function index() {
     }
 
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Text style={styles.appName}>Furnix</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    appName: {
+        fontFamily: 'Pacifico',
+        fontSize: 48,
+        color: '#545454'
+    }
+})
