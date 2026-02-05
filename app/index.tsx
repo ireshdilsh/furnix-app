7b1c4636743f75c5d46d5c37037c6cafa2dffe3d
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-05 22:41:31 +0530

    refactor: remove unused Authentication, Login, and Register screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { View, Image } from 'react-native'
-import React, { useEffect } from 'react'
-import { useRouter } from 'expo-router';
-import Header from '@/components/header';
+import { View } from 'react-native'
 
 export default function index() {
 
-    const router = useRouter()
-
-    useEffect(() => {
-        setTimeout(() => {
-            // @ts-ignore
-            router.replace("/screens/Authentication");
-        }, 5000);
-    });
-
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-            <Header />
-            <Image source={require('../assets/images/FURNIX-removebg-preview.png')} style={{ width: 150, height: 200 }} />
-        </View>
+            </View>
     )
 }
