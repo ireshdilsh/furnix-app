e3b24d23f7a63de734be652d57025a95d0081a16
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-05 23:43:17 +0530

    feat: add new variable and static font files for Josefin Sans and Pacifico

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,24 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect } from 'react';
 import { View } from 'react-native'
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function index() {
+
+    const [loaded, error] = useFonts({
+        'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
 
     return (
         <View>
