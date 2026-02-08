2089bcb6ce9523ac15ef0e70d39d23c4a9d09d14
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-08 18:36:55 +0530

    feat: enhance MainScreen layout with Header component and improve button styling

--- app/screens/MainScreen.tsx
+++ app/screens/MainScreen.tsx
@@ -1,8 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Touchable, TouchableOpacity } from 'react-native'
 import React, { useEffect } from 'react'
 import { Fonts } from '@/config/Fonts';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import Header from '@/components/header';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,9 +23,16 @@ export default function MainScreen() {
     }, [loaded, error]);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Where Style{'\n'}Meets Comfort</Text>
-            <Text style={styles.description}>Create beautiful spaces with designs that feel as good as they look.</Text>
+        <View>
+            <Header />
+            <View style={styles.container}>
+                <Text style={styles.title}>Where Style{'\n'}Meets Comfort</Text>
+                <Text style={styles.description}>Create beautiful spaces with designs that feel as good as they look.</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Get Started</Text>
+                    <AntDesign style={styles.buttonIcon} name="right-circle" size={15} color="white" />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -31,19 +40,47 @@ export default function MainScreen() {
 const styles = StyleSheet.create({
 
     container: {
-        paddingHorizontal: 45,
+        paddingHorizontal: 40,
     },
 
     title: {
         fontFamily: 'Josefin-Bold',
-        fontSize: 32,
+        fontSize: 30,
         color: '#4a5565',
         marginTop: 550,
+        textAlign: 'center'
     },
 
     description: {
-        marginTop: 5,
+        marginTop: 7,
         color: '#718096',
-        fontSize: 17
+        fontSize: 16,
+        textAlign: 'center',
+    },
+
+    button: {
+        backgroundColor: '#f68403',
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 25,
+        marginTop: 25,
+        alignSelf: 'center',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+        fontFamily: 'Josefin',
+        textAlign: 'center',
+    },
+
+    buttonIcon: {
+        marginTop: 5
     }
 })
