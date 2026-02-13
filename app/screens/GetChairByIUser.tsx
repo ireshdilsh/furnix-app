f69c214cf219d73d48d2ee7bc24ad9ebe84ceffc
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-13 23:14:28 +0530

    feat: update GetChairByIUser component layout and add .env configuration file

--- app/screens/GetChairByIUser.tsx
+++ app/screens/GetChairByIUser.tsx
@@ -1,14 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Header from '@/components/header'
 
 export default function GetChairByIUser() {
   return (
     <View>
-      <Text>GetChairByIUser</Text>
+      <Header />
+      <View style={styles.container}>
+
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+
+  container: {
+    paddingHorizontal: 30,
+    marginTop: 70,
+  }
+
 })
