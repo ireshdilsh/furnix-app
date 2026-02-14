715515d3821c319cdb376d35abb40e20ef653b46
ireshdilsh <ireshdilshan46@gmail.com>
2026-02-14 12:04:05 +0530

    feat: implement LandingScreen component and update index for font loading

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,34 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import LandingScreen from './screens/LandingScreen'
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function index() {
+
+    // import fonts
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [loaded, error] = useFonts({
+        'Josefin-medium': require('../fonts/josefin/static/JosefinSans-Medium.ttf'),
+        'Pacifico': require('../fonts/pacifico/Pacifico-Regular.ttf')
+    });
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+
     return (
         <View>
-            <Text>
-                Furnix
-            </Text>
+            <LandingScreen />
         </View>
     )
 }
